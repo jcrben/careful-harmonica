a13f40780152f6e79e27d0aa6be9dd09dbf11358
Ben Creasy <contact@bencreasy.com>
2015-03-15 09:47:22 -0700

    (feat) Start firebase integration

---
+++ app/services/services.js
@@ -0,0 +1,28 @@
+
+// Okay guys here's what I'm thinking for MVP:
+// 1. Dashboard page with task list and employer list
+// 2. Employer page with task list and completed task list
+// 3. User page with task list and completed task list
+/*
+getAllTasks - values 0
+getAllCompleteDTasks
+getEmployerList
+completeTaskForEmpployer
+addEmployer
+getTasksForEmployer
+getCompletedForEmployer
+
+isTaskCompleted - values 1 or 0
+
+-----
+foundJobPosition
+*/
+
+(function(){
+  'use strict';
+  angular
+    .module('app.services', ['firebase']);
+
+    .factory('AllUserData', [''])
+
+})
