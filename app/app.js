57dfdbdbc480fe0eb1c2bbfe0f8bef9281755ea2
Ben Creasy <contact@bencreasy.com>
2015-03-18 16:22:53 -0700

    (feat) Rename user -> users to complete leaderboard

--- app/app.js
+++ app/app.js
@@ -23,7 +23,7 @@ var app = angular
     'app.dashboard',
     'app.employer',
     'app.onboard',
-    'app.user',
+    'app.users',
     'app.land'
   ]);
 
@@ -70,16 +70,12 @@ app.config(function($stateProvider, $urlRouterProvider) {
     })
 
     // ********************** User Profile **********************
-    .state('user', {
-      url: '/user/:user',
+    .state('users', {
+      url: '/users/',
       views: {
         '': {
-          templateUrl: 'user/user.html',
-          controller: 'UserCtrl'
-        },
-        'tasks@user': {
-          templateUrl: 'user/tasks.html',
-          controller: 'UserCtrl'
+          templateUrl: 'users/users.html',
+          controller: 'UsersCtrl'
         }
       }
     })
